4198b4495da3a52309789ef04d7a2ac82f5c1890
Terrence Chung <terrencechung35@gmail.com>
2023-01-19 20:58:42 -0800

    lab

--- index.js
+++ index.js
@@ -1,1 +1,49 @@
 // Your code here
+const dodger = document.getElementById("dodger");
+function moveDodgerLeft() {
+  const leftNumbers = dodger.style.left.replace("px", "");
+  const left = parseInt(leftNumbers, 10);
+  if (left > 0) {
+    dodger.style.left = `${left - 1}px`;
+  }
+}
+function moveDodgerRight() {
+  const leftNumbers = dodger.style.left.replace("px", "");
+  const left = parseInt(leftNumbers, 10);
+  if (left < 360) {
+    dodger.style.left = `${left + 1}px`;
+  }
+  document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowLeft") {
+      moveDodgerLeft();
+    }
+    document.addEventListener("keydown", function (e) {
+      if (e.key === "ArrowRight") {
+        moveDodgerRight();
+      }
+    });
+  });
+}
+
+
+
+// dodger.style.backgroundColor = "#FF69B4";
+// dodger.style.bottom = "0px";
+// dodger.style.left = "0px";
+
+// function moveDodgerLeft() {
+//   const leftNumbers = dodger.style.left.replace("px", "");
+//   const left = parseInt(leftNumbers, 10);
+
+//   if (left > 0) {
+//     dodger.style.left = `${left - 1}px`;
+//   }
+// }
+
+// const dodger = document.getElementById("dodger");
+
+// document.addEventListener("keydown", function (e) {
+//   if (e.key === "ArrowLeft") {
+//     moveDodgerLeft();
+//   }
+// });
